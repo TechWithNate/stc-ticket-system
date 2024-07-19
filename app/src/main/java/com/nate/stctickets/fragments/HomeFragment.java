227a539edeb0830e3026d3a2f1722f1f8c86f107
NathanDzreke-Poku <dzrekenathan2002@gmail.com>
2024-07-19 14:29:19 +0000

    Initial Commit

--- app/src/main/java/com/nate/stctickets/fragments/HomeFragment.java
+++ app/src/main/java/com/nate/stctickets/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.nate.stctickets.fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +14,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.denzcoskun.imageslider.ImageSlider;
@@ -20,23 +25,33 @@ import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nate.stctickets.R;
 import com.nate.stctickets.activities.BookTicket;
+import com.nate.stctickets.activities.TicketDetails;
+import com.nate.stctickets.adapters.BookingAdapter;
+import com.nate.stctickets.models.BookingModel;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements BookingAdapter.TicketClicked{
 
     private View view;
     private MaterialButton bookBtn;
     private ImageSlider slider;
     private TextView username;
+    private TextView seeAllTickets;
+    private RecyclerView ticketRecycler;
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseReference;
     private ImageView profileImage;
+    private ArrayList<BookingModel> bookingModels;
+    private BookingAdapter adapter;
 
 
     @Nullable
@@ -56,7 +71,23 @@ public class HomeFragment extends Fragment {
         slider.setImageList(imageList, ScaleTypes.FIT);
         loadUserData();
 
+        seeAllTickets.setOnClickListener(v -> navigateBookings());
+
+        ticketRecycler = view.findViewById(R.id.ticket_recycler);
+        ticketRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        ticketRecycler.setHasFixedSize(true);
+        bookingModels = new ArrayList<>();
+        adapter = new BookingAdapter(bookingModels, getContext(), this);
+        ticketRecycler.setAdapter(adapter);
+
+
+        fetchBookingModel();
+
         return view;
+    }
+
+    private void navigateBookings() {
+        openFragment(new BookingsFragment());
     }
 
     private void initViews() {
@@ -64,6 +95,8 @@ public class HomeFragment extends Fragment {
         slider = view.findViewById(R.id.image_slider);
         username = view.findViewById(R.id.username);
         firebaseAuth = FirebaseAuth.getInstance();
+        seeAllTickets = view.findViewById(R.id.see_all);
+        ticketRecycler = view.findViewById(R.id.ticket_recycler);
         databaseReference = FirebaseDatabase.getInstance().getReference("users");
         profileImage = view.findViewById(R.id.profile_img);
     }
@@ -84,6 +117,51 @@ public class HomeFragment extends Fragment {
                 Toast.makeText(getActivity(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             });
         }
+    }
+
+    private void openFragment(Fragment fragment){
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void fetchBookingModel() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(firebaseAuth.getUid()).child("tickets");
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    BookingModel model = snapshot.getValue(BookingModel.class);
+                    if (model != null){
+                        bookingModels.add(model);
+                    }else {
+                        Toast.makeText(getContext(), "Booking not found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onTicketClicked(int position) {
+        viewTicketDetails(bookingModels.get(position));
+    }
+
+    private void viewTicketDetails(BookingModel bookingModel) {
+        Intent intent = new Intent(getContext(), TicketDetails.class);
+        intent.putExtra("ticket", bookingModel);
+        startActivity(intent);
     }
 
 
